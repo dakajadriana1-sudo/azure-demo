b63523984ec0e1474eb3a64a92e76a2752df4fa7
Dakaj Adriána <adakaj@trask.cz>
2026-02-07 21:32:49 +0100

    Add terraform configuration

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "rg-terraform-state-dev"
+        storage_account_name = "sttfstatemax1dev"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+        use_oidc            = true
+        use_azuread_auth    = true
+    }
+}
